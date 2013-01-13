a759a21c43269c499b86bd0cc3951f36792fe8ec
Joseph Lee <leejonggun0129@gmail.com>
2013-01-14 06:25:33 +0900

    add some ds scripts for DCase

--- Diagnosis/ICMP_accept.sh
+++ Diagnosis/ICMP_accept.sh
@@ -6,9 +6,10 @@ sudo iptables -P INPUT DROP
 sudo iptables -P OUTPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
-sudo iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+sudo iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 sudo iptables -L
 
 echo "ping Test"
-ping www.google.com -c 3
+ping 192.168.1.1 -c 2
+ping www.google.com -c 2
 echo "Test End"
--- Diagnosis/ICMP_block.sh
+++ Diagnosis/ICMP_block.sh
@@ -6,9 +6,10 @@ sudo iptables -P INPUT ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -A OUTPUT -p icmp --icmp-type echo-request -j DROP
-sudo iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+sudo iptables -A INPUT -p icmp --icmp-type echo-reply -j DROP
 sudo iptables -L
 
 echo "ping Test"
+ping 192.168.1.1 -c 2
 ping www.google.com -c 3
 echo "Test End"
